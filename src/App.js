49efd8a69a0fc49c84757ed3604e6278d1d808ff
branfung <brandon.fung@upr.edu>
2021-07-08 20:49:32 -0400

    aws boilerplate set up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify,  { Auth } from "aws-amplify"
+import awsconfig from "./aws-exports"
 
-function App() {
+Amplify.configure(awsconfig) //Base React App with AWS Amplify configured
+//decided to create my own Auth UI instead of the prebuilt one that Amplify provides 
+
+function App() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+
       </header>
     </div>
   );
